7299e6a6255467174b3e0613641981fbb5a425e7
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-05-13 14:41:05 +0200

    Added a bunch of files from previous project

---
+++ BusinessApplication/Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace BusinessApplication.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetAll();
+        List<T> Find(Expression<Func<T, bool>> searchTerm);
+        Task AddAsync(T entity);
+        void Remove(T entity);
+        void Update(T entity);
+    }
+}
---
+++ BusinessApplication/Repository/Repository.cs
@@ -0,0 +1,135 @@
+﻿using BusinessApplication.Model;
+using BusinessApplicationProject;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BusinessApplication.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly AppDbContext Context;
+
+        public Repository(AppDbContext context)
+        {
+            Context = context;
+        }
+
+        public List<T> GetAll()
+        {
+            if (Context.Database.CanConnect())
+            {
+                var query = AddIncludes(typeof(T), Context.Set<T>());
+                return query.ToList();
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        public List<T> Find(Expression<Func<T, bool>> condition)
+        {
+            if (Context.Database.CanConnect())
+            {
+                var query = AddIncludes(typeof(T), Context.Set<T>());
+                return [.. query.Where(condition)];
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            if (await Context.Database.CanConnectAsync())
+            {
+                await Context.Set<T>().AddAsync(entity);
+                await Context.SaveChangesAsync();
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+
+        }
+
+        public void Remove(T entity)
+        {
+            if (Context.Database.CanConnect())
+            {
+                Context.Set<T>().Remove(entity);
+                Context.SaveChanges();
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        public void Update(T entity)
+        {
+            if (Context.Database.CanConnect())
+            {
+                Context.Set<T>().Update(entity);
+                Context.SaveChanges();
+            }
+            else
+            {
+                throw new TimeoutException();
+            }
+        }
+
+        private IQueryable<T> AddIncludes(Type type, IQueryable<T> query)
+        {
+            var includes = GetAllNavigationPropertyNames(typeof(T));
+
+            foreach (var propertyName in includes)
+            {
+                query = query.Include(propertyName);
+            }
+
+            return query;
+        }
+
+        private List<string> GetAllNavigationPropertyNames(Type type)
+        {
+            var res = new List<string>();
+
+            switch (type.Name)
+            {
+                case nameof(Article):
+                    res.Add("Group.Parent");
+                    break;
+
+                case nameof(ArticleGroup):
+                    res.Add("Parent");
+                    break;
+
+                case nameof(Customer):
+                    res.Add("CustomerAddress");
+                    break;
+
+                case nameof(Invoice):
+                    res.Add("BillingAddress");
+                    res.Add("OrderInformations.CustomerDetails.CustomerAddress");
+                    res.Add("OrderInformations.Positions.ArticleDetails.Group.Parent");
+                    break;
+
+                case nameof(Order):
+                    res.Add("CustomerDetails.CustomerAddress");
+                    res.Add("Positions.ArticleDetails.Group.Parent");
+                    break;
+
+                case nameof(Position):
+                    res.Add("ArticleDetails.Group.Parent");
+                    break;
+
+                default:
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
