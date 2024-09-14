ad171f41c269fc748e17b7f1a959681451f555db
Elias G <83478605+eliasgoette@users.noreply.github.com>
2024-09-14 19:55:44 +0200

    Created tests for password encryption utility class

---
+++ BusinessApplication.Tests/PasswordEncryptionUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using BusinessApplication.Utility;
+
+namespace BusinessApplication.Tests
+{
+    [TestClass]
+    public class PasswordEncryptionUnitTest
+    {
+        [TestMethod]
+        public void EncryptsPasswordsWithSpecialCharacters()
+        {
+            var passwd = "^!§\"$%&/()=?`´\\{}[]";
+            var res = PasswordEncryption.HashPassword(passwd);
+            Assert.IsNotNull(res);
+        }
+
+        [TestMethod]
+        public void LengthCanNotBeDerivedFromHash()
+        {
+            var shortPasswd = "asdf";
+            var longPasswd = "dsuifhsiofhidhffbsilfidfoifbfoinoiscnpdnifsofiosijfij";
+
+            var shortHash = PasswordEncryption.HashPassword(shortPasswd);
+            var longHash = PasswordEncryption.HashPassword(longPasswd);
+
+            Assert.AreEqual(shortHash?.Length, longHash?.Length);
+        }
+
+        [TestMethod]
+        public void HashMethodIsNotDeterministic()
+        {
+            var passwd = "Pa$$w0rd";
+
+            var res1 = PasswordEncryption.HashPassword(passwd);
+            var res2 = PasswordEncryption.HashPassword(passwd);
+
+            Assert.AreNotEqual(res1, res2);
+        }
+    }
+}
